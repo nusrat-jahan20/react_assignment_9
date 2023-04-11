2a795689ab78bc9a452d82af9576f25082ddbe00
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2023-04-11 16:07:37 +0600

    created home page banner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ import ActiveLink from '../ActiveLink/ActiveLink';
 const Header = () => {
     return (
         <nav>
-            <ActiveLink to="/">Home</ActiveLink>
+            <ActiveLink to="/">CareerCrate</ActiveLink>
             <ActiveLink to="/statistics">Statistics</ActiveLink>
-            <ActiveLink to="/job_details">Job details</ActiveLink>
+            {/* <ActiveLink to="/job_details">Job details</ActiveLink> */}
             <ActiveLink to="/applied_jobs">Applied Jobs</ActiveLink>
             <ActiveLink to="/blogs">Blogs</ActiveLink>
         </nav>
---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h1>Looking for Your Dream Job?</h1>
+            <p>
+                Find thousands of job circulars from top companies, and apply to your dream job in
+                just a few clicks! CareerCrate is your ultimate job search partner, providing you
+                with the latest and most relevant job opportunities.
+            </p>
+            <button>Get Hired!</button>
+            <img
+                src="https://img.freepik.com/free-photo/pajamas-clothing-wear-cheerful-adult_1368-2051.jpg?w=1060&t=st=1681207560~exp=1681208160~hmac=ec7b3ca9daaa8e78109bc3fe96e8da634dbf97a7fa7ad7ccf51f37e6dbdfe2b3"
+                alt=""
+            />
+        </div>
+    );
+};
+
+export default Banner;
